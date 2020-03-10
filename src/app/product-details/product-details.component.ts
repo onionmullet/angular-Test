ef16f11ebee8db58c84839a730cb5688318ce982
onionmullet <afrankli@hotmail.com>
2020-03-10 12:23:24 -0500

    3 files modified, 3 added, Added buy functionality, start cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,18 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
+
 export class ProductDetailsComponent implements OnInit {
+
 
   product;
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
+
+  /*  define cart function for service
+  *  Receives the current product.
+  *  Uses the cart service's addToCart() method to add the product the cart.
+  *  Displays a message that you've added a product to the cart.
+  */
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
